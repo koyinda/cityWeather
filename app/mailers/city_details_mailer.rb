1e7d1131d41765ee5f8b18eff16a1fc6b9c40421
koyinda <oyinda.kosemani@gmail.com>
2020-11-24 08:32:41 +0100

    updated mail

--- app/mailers/city_details_mailer.rb
+++ app/mailers/city_details_mailer.rb
@@ -2,6 +2,6 @@ class CityDetailsMailer < ApplicationMailer
     default from: 'oyindamola.kosemani@gmail.com'
   def submission(message, toWho)
       @message = message
-      mail(to: 'oyinda.kosemani@gmail.com', subject: 'City Weather Details for the Day')
+      mail(to: toWho, subject: 'City Weather Details for the Day')
     end
   end
