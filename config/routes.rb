be2ab94f21ada7711011e0ac3d7673d480b612a8
koyinda <oyinda.kosemani@gmail.com>
2020-11-25 11:21:03 +0100

    config for sidekiq and redis

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,5 @@
+require 'sidekiq/web'
+
 Rails.application.routes.draw do
   mount GraphiQL::Rails::Engine, at: "/graphiql", graphql_path: "/graphql"
 
@@ -9,5 +11,11 @@ Rails.application.routes.draw do
   post "/graphql", to: "graphql#execute"
 
   post 'mail/message', to: "mail#create"
+
+  Sidekiq::Web.use Rack::Auth::Basic do |username, password|
+    ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(username), ::Digest::SHA256.hexdigest(ENV['SIDEKIQ_USERNAME'])) &
+      ActiveSupport::SecurityUtils.secure_compare(::Digest::SHA256.hexdigest(password), ::Digest::SHA256.hexdigest(ENV['SIDEKIQ_PASSWORD']))
+  end
+  mount Sidekiq::Web, at: '/sidekiq'
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
