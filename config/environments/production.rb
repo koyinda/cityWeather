39c37ac4cdeb2fb76b4d115ae35c06adc6b1dd7b
koyinda <oyinda.kosemani@gmail.com>
2020-11-25 12:17:18 +0100

    graphql display fix

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -27,7 +27,8 @@ Rails.application.configure do
 
   # Do not fallback to assets pipeline if a precompiled asset is missed.
   config.assets.compile = false
-
+  config.assets.debug = false
+  config.assets.unknown_asset_fallback = true
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.action_controller.asset_host = 'http://assets.example.com'
 
